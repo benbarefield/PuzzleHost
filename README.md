de55c64656f328cae22070c08293a058fe9cdf6d
Ben Barefield <ben.barefield@gmail.com>
2024-10-14 14:13:13 -0700

    cors and auth stubs

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Getting started
-Run `npm i`
+Run `npm i` from both the `/client` and `/server`
+
+To run locally (after fulfilling any below requirements):
+* `npm run dev` in `/client`
+* `npm run build` and `npm run serve` in `/server`
 
 ## .env
 Create a `.env` file in `/server` with the following entries:
 
-* `PG_HOST` : the uri for postgres
-* `PG_USER=` : the postgres user to use in your connections
-* `PG_PASSWORD=` : the password for your postgres user
+* `PG_CONNECTION` : the connection string for postgres, eg. `postgres://user:password@localhost:5432/postgres`
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,29 @@
 import 'dotenv/config';
 import express from "express";
-import puzzle from './endpoints/puzzle';
-import userPuzzles from "./endpoints/userPuzzles";
 import sessionStarter from "./data/sessionStarter";
+import setupServer from "./serverSetup";
+import authorization from "./authorization";
 
 const port = 8888;
 const app = express();
 
 (async function() {
-  app.use(express.json());
-  app.use(express.urlencoded({extended: false}));
-  // app.use(authenticator);
+  // todo: fix up cors.
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", ["*"]);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+  });
+  setupServer(app, authorization);
 
   try {
-    await sessionStarter(app);
+    let dbClient = await sessionStarter(app);
   }
   catch(e) {
     console.log("error connecting to database:", e);
     return;
   }
-
-  app.all("puzzle/:id?", puzzle);
-  app.all("userPuzzles", userPuzzles);
 
   app.listen(port, () => {
     console.log(`Server started on port: ${port}
